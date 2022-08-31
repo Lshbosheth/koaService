2dd3588cb3c7522b8e86b00795e217b502722479
lizezhi <lizezhi@jk366.net>
2022-08-31 20:43:29 +0800

    fix

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: '124.221.188.77',
     user: 'root',
     password: 'NAna-0218',
     database: 'test',
