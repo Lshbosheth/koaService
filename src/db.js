3b34cb438950327ec45e0096b79f56378502821b
lizezhi <lizezhi@jk366.net>
2022-08-31 18:14:58 +0800

    one result

---
+++ src/db.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: '124.221.188.77',
+    user: 'root',
+    password: 'NAna-0218',
+    database: 'test',
+    multipleStatements: true
+})
+connection.connect()
+connection.query('select 1 + 1', (err, rows) => { /* */ });
+const query = function (sql) {
+  return new Promise((resolve, reject) => {
+      connection.query(sql,  function (error, results) {
+          if(error){
+              reject(error);
+          }else{
+              resolve(results)
+          }
+      });
+  })
+}
+async function getAll() {
+    return await query("SELECT * FROM user");
+  }
+
+
+
+module.exports = {
+    getAll
+  };
