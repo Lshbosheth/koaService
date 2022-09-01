2332b8a269ce29637304732fea6327ca24174bd8
lizezhi <lizezhi@jk366.net>
2022-09-01 20:19:27 +0800

    reset

--- src/db.js
+++ src/db.js
@@ -1,14 +1,13 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: '124.221.188.77',
     user: 'root',
-    password: '',
+    password: 'NAna-0218',
     database: 'test',
     multipleStatements: true
 })
 connection.connect()
-
 connection.query('select 1 + 1', (err, rows) => { /* */ });
 const query = function (sql) {
   return new Promise((resolve, reject) => {
